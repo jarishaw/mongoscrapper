9ebfeaf87ea22d46b64402bd6759c8c68a8145de
jarishaw <jarishaw@gmail.com>
2018-02-26 20:19:37 -0600

    update database

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.use(express.static(__dirname + "/public"));
 // By default mongoose uses callbacks for async queries, we're setting it to use promises (.then syntax) instead
 // Connect to the Mongo DB
 
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/nytimesarticle";
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/nytimesarticles";
 
 mongoose.Promise = Promise;
 mongoose.connect(MONGODB_URI, {
